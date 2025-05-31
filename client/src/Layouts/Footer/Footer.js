03bed2785852d11addbd68cc5c02c980fb8fe11d
hoangquach243 <151592391+hoangquach243@users.noreply.github.com>
2025-06-01 03:00:10 +0700

    Cải thiện giao diện: Thiết kế lại Header và Footer với giao diện hiện đại hơn, thêm thông tin pháp lý và các mục khác

--- client/src/Layouts/Footer/Footer.js
+++ client/src/Layouts/Footer/Footer.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Footer.module.scss';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import {
+    faEnvelope,
+    faLocationDot,
+    faPhone,
+    faClock,
+    faShieldHalved,
+    faTruck,
+    faFileLines,
+    faCircleQuestion,
+} from '@fortawesome/free-solid-svg-icons';
+
+import BCTLogo from './img/bct.png';
+import SSLLogo from './img/ssl.png';
+import PaymentLogo from './img/payment.png';
 
 const cx = classNames.bind(styles);
 
@@ -8,46 +25,117 @@ function Footer() {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <main className={cx('form-list-footer')}>
-                    <div className={cx('column-footer')}>
-                        <h4>Shop Men</h4>
-                        <ul>
-                            <li>Clothing Fashion</li>
-                            <li>Winter</li>
-                            <li>Sumer</li>
-                            <li>Formal</li>
-                            <li>Casual</li>
+                <div className={cx('footer-content')}>
+                    {/* Thông tin pháp lý */}
+                    <div className={cx('footer-column')}>
+                        <h3 className={cx('footer-title')}>Mao Cloth</h3>
+                        <ul className={cx('footer-info')}>
+                            <li>Công ty TNHH Thời Trang Mao Cloth</li>
+                            <li>Mã số thuế: 0123456789</li>
+                            <li>Số ĐKKD: 0123456789 do Sở KH&ĐT TP.HCM cấp ngày 01/01/2023</li>
+                            <li>Đã thông báo với Bộ Công Thương</li>
                         </ul>
+                        <div className={cx('footer-certificates')}>
+                            <img src={BCTLogo} alt="Bộ Công Thương" />
+                            <img src={SSLLogo} alt="SSL Certificate" />
+                        </div>
                     </div>
-                    <div className={cx('column-footer')}>
-                        <h4>Shop Women</h4>
-                        <ul>
-                            <li>Clothing Fashion</li>
-                            <li>Winter</li>
-                            <li>Sumer</li>
-                            <li>Formal</li>
-                            <li>Casual</li>
-                        </ul>
-                    </div>
-                    <div className={cx('column-footer')}>
-                        <h4>Other Collection</h4>
-                        <ul>
-                            <li>Nước hoa</li>
-                            <li>Nến thơm</li>
-                            <li>Son</li>
+
+                    {/* Thông tin liên hệ */}
+                    <div className={cx('footer-column')}>
+                        <h3 className={cx('footer-title')}>Thông tin liên hệ</h3>
+                        <ul className={cx('footer-contact')}>
+                            <li>
+                                <FontAwesomeIcon icon={faPhone} />
+                                <span>Hotline: 1900 1234 (8:00 - 21:00)</span>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faEnvelope} />
+                                <span>Email: cskh@maocloth.com</span>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faLocationDot} />
+                                <span>227 Nguyễn Văn Cừ, Phường 4, Quận 5, TP.HCM</span>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faClock} />
+                                <span>Giờ làm việc: 8:00 - 21:00 (Tất cả các ngày)</span>
+                            </li>
                         </ul>
                     </div>
 
-                    <div className={cx('column-footer')}>
-                        <h4>Quick Links</h4>
-                        <ul>
-                            <li>Facebook</li>
-                            <li>Instagram</li>
-                            <li>X</li>
-                            <li>Zalo</li>
+                    {/* Chính sách quan trọng */}
+                    <div className={cx('footer-column')}>
+                        <h3 className={cx('footer-title')}>Chính sách</h3>
+                        <ul className={cx('footer-links')}>
+                            <li>
+                                <FontAwesomeIcon icon={faShieldHalved} />
+                                <Link to="/chinh-sach-doi-tra">Chính sách đổi trả và bảo hành</Link>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faTruck} />
+                                <Link to="/chinh-sach-van-chuyen">Chính sách vận chuyển</Link>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faFileLines} />
+                                <Link to="/chinh-sach-bao-mat">Chính sách bảo mật</Link>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faFileLines} />
+                                <Link to="/dieu-khoan-su-dung">Điều khoản sử dụng</Link>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faCircleQuestion} />
+                                <Link to="/huong-dan-mua-hang">Hướng dẫn mua hàng</Link>
+                            </li>
                         </ul>
                     </div>
-                </main>
+
+                    {/* Liên kết hữu ích */}
+                    <div className={cx('footer-column')}>
+                        <h3 className={cx('footer-title')}>Liên kết hữu ích</h3>
+                        <ul className={cx('footer-links')}>
+                            <li>
+                                <Link to="/sitemap">Sơ đồ trang web</Link>
+                            </li>
+                            <li>
+                                <Link to="/blog">Blog thời trang</Link>
+                            </li>
+                            <li>
+                                <Link to="/huong-dan-chon-size">Hướng dẫn chọn size</Link>
+                            </li>
+                            <li>
+                                <Link to="/faq">Câu hỏi thường gặp</Link>
+                            </li>
+                        </ul>
+
+                        <h3 className={cx('footer-title', 'social-title')}>Kết nối với chúng tôi</h3>
+                        <div className={cx('footer-social')}>
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faFacebookF} />
+                            </a>
+                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faInstagram} />
+                            </a>
+                            <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faTiktok} />
+                            </a>
+                            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faYoutube} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx('footer-bottom')}>
+                    <div className={cx('payment-methods')}>
+                        <p>Phương thức thanh toán</p>
+                        <img src={PaymentLogo} alt="Phương thức thanh toán" />
+                    </div>
+                    <div className={cx('copyright')}>
+                        <p>&copy; {new Date().getFullYear()} Mao Cloth. Tất cả các quyền được bảo lưu.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- client/src/Layouts/Header/Header.js
+++ client/src/Layouts/Header/Header.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-import Logo from './img/logo.png';
+import Logo from './img/logo2.png';
 import request from '../../config/Connect';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faBars, faCartPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react';
-
 import { Link } from 'react-router-dom';
 import useDebounce from '../../customHook/useDebounce';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faBars, faCartShopping, faSearch, faXmark, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
 const cx = classNames.bind(styles);
 
 function Header() {
@@ -19,12 +18,18 @@ function Header() {
     const [showMenu, setShowMenu] = useState(false);
     const [dataSearch, setDataSearch] = useState([]);
     const [searchValue, setSearchValue] = useState('');
-    const [checkHeader, setCheckHeader] = useState(5);
+    const [activeMenu, setActiveMenu] = useState('/');
+    const [showSearch, setShowSearch] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
 
     const debounce = useDebounce(searchValue, 500);
 
     const handleShowMenu = () => {
         setShowMenu(!showMenu);
+    };
+
+    const handleShowSearch = () => {
+        setShowSearch(!showSearch);
     };
 
     useEffect(() => {
@@ -38,102 +43,131 @@ function Header() {
         }
     }, [debounce]);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // Set active menu based on current path
+        setActiveMenu(window.location.pathname);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className={cx('wrapper')}>
-            <Link style={{ textDecoration: 'none' }} to="/">
-                <div className={cx('logo')}>
-                    <img src={Logo} alt="" />
-                    <h1>Apex Fashion</h1>
+        <div className={cx('wrapper', { scrolled })}>
+            <div className={cx('header-container')}>
+                <div className={cx('mobile-toggle')} onClick={handleShowMenu}>
+                    <FontAwesomeIcon icon={showMenu ? faXmark : faBars} />
                 </div>
-            </Link>
 
-            <div className={cx('')}>
-                <div className={cx('input-search')}>
-                    <input onChange={(e) => setSearchValue(e.target.value)} />
-                    <FontAwesomeIcon icon={faSearch} style={{ paddingRight: '15px' }} />
-                </div>
-                <div className={cx('search-result')}>
-                    {dataSearch.length > 0 && searchValue ? (
-                        <div className={cx('result')}>
-                            {dataSearch.map((item) => (
-                                <Link to={`/prodetail/${item?.id}`} key={item?._id} id={cx('test')}>
-                                    <div className={cx('form-result')}>
-                                        <img id={cx('img-result')} src={`http://localhost:5001/${item?.img}`} alt="" />
-                                        <h5>{item?.nameProducts}</h5>
-                                        <span>{item?.priceNew?.toLocaleString()} đ</span>
-                                    </div>
-                                </Link>
-                            ))}
+                <Link to="/" className={cx('logo')}>
+                    <img src={Logo} alt="Mao Cloth Logo" />
+                    <h1>Mao Cloth</h1>
+                </Link>
+
+                <nav className={cx('navigation', { 'show-menu': showMenu })}>
+                    <ul>
+                        <li className={cx({ active: activeMenu === '/' })}>
+                            <Link to="/">Trang Chủ</Link>
+                        </li>
+                        <li className={cx({ active: activeMenu === '/category' })}>
+                            <Link to="/category">Sản Phẩm</Link>
+                            <FontAwesomeIcon icon={faAngleDown} className={cx('dropdown-icon')} />
+                            <div className={cx('dropdown-menu')}>
+                                <Link to="/category?type=men">Nam</Link>
+                                <Link to="/category?type=women">Nữ</Link>
+                                <Link to="/category?type=accessories">Phụ kiện</Link>
+                            </div>
+                        </li>
+                        <li className={cx({ active: activeMenu === '/aboutus' })}>
+                            <Link to="/aboutus">Giới Thiệu</Link>
+                        </li>
+                        <li className={cx({ active: activeMenu === '/contact' })}>
+                            <Link to="/contact">Liên Hệ</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className={cx('header-actions')}>
+                    <div className={cx('search-container')}>
+                        <button className={cx('action-btn')} onClick={handleShowSearch}>
+                            <FontAwesomeIcon icon={faSearch} />
+                        </button>
+
+                        <div className={cx('search-panel', { active: showSearch })}>
+                            <div className={cx('search-input')}>
+                                <input
+                                    type="text"
+                                    placeholder="Tìm kiếm sản phẩm..."
+                                    value={searchValue}
+                                    onChange={(e) => setSearchValue(e.target.value)}
+                                />
+                                <button>
+                                    <FontAwesomeIcon icon={faSearch} />
+                                </button>
+                                <button className={cx('close-search')} onClick={handleShowSearch}>
+                                    <FontAwesomeIcon icon={faXmark} />
+                                </button>
+                            </div>
+
+                            {dataSearch.length > 0 && searchValue && (
+                                <div className={cx('search-results')}>
+                                    {dataSearch.map((item) => (
+                                        <Link
+                                            to={`/prodetail/${item?.id}`}
+                                            key={item?._id}
+                                            className={cx('search-item')}
+                                        >
+                                            <div className={cx('search-item-image')}>
+                                                <img
+                                                    src={`http://localhost:5001/${item?.img}`}
+                                                    alt={item?.nameProducts}
+                                                />
+                                            </div>
+                                            <div className={cx('search-item-info')}>
+                                                <h5>{item?.nameProducts}</h5>
+                                                <span>{item?.priceNew?.toLocaleString()} đ</span>
+                                            </div>
+                                        </Link>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                    ) : (
-                        <></>
-                    )}
-                </div>
-            </div>
+                    </div>
 
-            <div onClick={handleShowMenu} id={cx('btn-menu')}>
-                <span>Menu</span>
-                <FontAwesomeIcon icon={faBars} />
-            </div>
-
-            <div className={cx('controller')}>
-                <ul>
-                    <Link
-                        style={{ textDecoration: 'none', color: '#333' }}
-                        to="/category"
-                        onClick={() => setCheckHeader(0)}
-                    >
-                        <li className={cx(checkHeader === 0 ? 'checkHeader' : '')}>Sản Phẩm</li>
+                    <Link to="/wishlist" className={cx('action-btn')}>
+                        <FontAwesomeIcon icon={faHeart} />
                     </Link>
 
-                    <Link
-                        style={{ textDecoration: 'none', color: '#333' }}
-                        to="/aboutus"
-                        onClick={() => setCheckHeader(1)}
-                    >
-                        <li className={cx(checkHeader === 1 ? 'checkHeader' : '')}>About Us</li>
-                    </Link>
-                    <Link
-                        style={{ textDecoration: 'none', color: '#333' }}
-                        to="/contact"
-                        onClick={() => setCheckHeader(2)}
-                    >
-                        <li className={cx(checkHeader === 2 ? 'checkHeader' : '')}>Liên Hệ</li>
-                    </Link>
-                    <Link
-                        style={{ textDecoration: 'none', color: '#333' }}
-                        to="/checkout"
-                        onClick={() => setCheckHeader(3)}
-                    >
-                        <li className={cx(checkHeader === 3 ? 'checkHeader' : '')}>Thanh Toán</li>
+                    <Link to="/cart" className={cx('action-btn', 'cart-btn')}>
+                        <FontAwesomeIcon icon={faCartShopping} />
+                        <span className={cx('cart-count')}>0</span>
                     </Link>
 
                     {token ? (
-                        <>
-                            <div className={cx('controller-user')}>
-                                <Link to="/cart">
-                                    <button>
-                                        <FontAwesomeIcon icon={faCartPlus} />
-                                    </button>
-                                </Link>
-                                <Link to={token ? '/info' : '/login'}>
-                                    <button>
-                                        <FontAwesomeIcon icon={faUser} />
-                                    </button>
-                                </Link>
-                            </div>
-                        </>
+                        <Link to="/info" className={cx('action-btn')}>
+                            <FontAwesomeIcon icon={faUser} />
+                        </Link>
                     ) : (
-                        <>
-                            <Link style={{ textDecoration: 'none', color: '#333' }} to={token ? '/info' : '/login'}>
-                                <li>Đăng Nhập</li>
-                            </Link>{' '}
-                            <Link style={{ textDecoration: 'none', color: '#333' }} to={'/register'}>
-                                <li>Đăng Ký</li>
+                        <div className={cx('auth-buttons')}>
+                            <Link to="/login" className={cx('login-btn')}>
+                                Đăng Nhập
                             </Link>
-                        </>
+                            <Link to="/register" className={cx('register-btn')}>
+                                Đăng Ký
+                            </Link>
+                        </div>
                     )}
-                </ul>
+                </div>
             </div>
         </div>
     );

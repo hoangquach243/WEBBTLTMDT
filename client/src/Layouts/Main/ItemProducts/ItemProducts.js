588cab720062028d456c2dae788ca94b48be004f
StormT <thinh.letruong@hcmut.edu.vn>
2025-06-01 11:04:23 +0700

    Update Main and ItemProducts components

--- client/src/Layouts/Main/ItemProducts/ItemProducts.js
+++ client/src/Layouts/Main/ItemProducts/ItemProducts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ItemProducts.module.scss';
 
@@ -6,29 +6,74 @@ import img1 from './img/items1.jpg';
 import img2 from './img/items2.jpg';
 import img3 from './img/items3.jpg';
 import img4 from './img/items4.jpeg';
+
 const cx = classNames.bind(styles);
 
-function ItemProducts() {
+const defaultProducts = [
+    { img: img1, name: 'Son' },
+    { img: img2, name: 'Giày' },
+    { img: img3, name: 'Nến Thơm' },
+    { img: img4, name: 'Nước Hoa' },
+];
+
+const VISIBLE_COUNT = 4;
+
+function ItemProducts({ dataProducts }) {
+    const isDynamic = Array.isArray(dataProducts) && dataProducts.length > 0;
+
+    const productsToShow = isDynamic
+        ? dataProducts.map((item) => ({
+              _id: item._id,
+              img: `http://localhost:5000/${item.img}`,
+              name: item.nameProducts,
+          }))
+        : defaultProducts.map((item, index) => ({
+              _id: index,
+              img: item.img,
+              name: item.name,
+          }));
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const totalPages = Math.ceil(productsToShow.length / VISIBLE_COUNT) || 1;
+
+    const handleNext = () => {
+        setCurrentIndex((prev) => (prev + 1 < totalPages ? prev + 1 : 0));
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((prev) => (prev - 1 >= 0 ? prev - 1 : totalPages - 1));
+    };
+
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('row-product')}>
-                <img id={cx('img-item')} src={img1} alt="" />
-                <span>Son</span>
+            <button className={cx('arrow', 'left')} onClick={handlePrev}>
+                &#8592;
+            </button>
+
+            <div className={cx('slider')}>
+                <div
+                    className={cx('slider-inner')}
+                    style={{
+                        transform: `translateX(-${currentIndex * 100}%)`,
+                        width: `${(80/ VISIBLE_COUNT) * productsToShow.length}%`,
+                    }}
+                >
+                    {productsToShow.map((product) => (
+                        <div key={product._id} className={cx('row-product')}>
+                            <img
+                                id={cx('img-item')}
+                                src={product.img}
+                                alt={product.name}
+                            />
+                            <span>{product.name}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
 
-            <div className={cx('row-product')}>
-                <img id={cx('img-item')} src={img2} alt="" />
-                <span>Giày</span>
-            </div>
-
-            <div className={cx('row-product')}>
-                <img id={cx('img-item')} src={img3} alt="" />
-                <span>Nến Thơm</span>
-            </div>
-            <div className={cx('row-product')}>
-                <img id={cx('img-item')} src={img4} alt="" />
-                <span>Nước Hoa</span>
-            </div>
+            <button className={cx('arrow', 'right')} onClick={handleNext}>
+                &#8594;
+            </button>
         </div>
     );
 }
